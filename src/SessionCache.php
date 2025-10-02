7f2637ea012cfc01aa8c9b948c5ce4194d594f95
Marco Monteiro <kanxuk@gmail.com>
2025-10-02 15:14:42 +0100

    updated SessionCache class

--- src/SessionCache.php
+++ src/SessionCache.php
@@ -7,12 +7,12 @@ class SessionCache {
     
     public static function getInstance(){ return empty(self::$instance)?(new self()):self::$instance; }
 
-	public static function cacheExpire(?int $value = null): int|false
+	public static function expire(?int $value = null): int|false
 	{
 		return session_cache_expire($value);
 	}
 
-	public static function cacheLimiter(?string $value = null): string|false
+	public static function limiter(?string $value = null): string|false
 	{
 		return session_cache_limiter($value);
 	}
